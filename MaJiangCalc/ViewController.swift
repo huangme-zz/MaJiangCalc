0c73d9cf673ff5b8c6884225c75812968ccde489
Meng Huang <yuyeguaike@gmail.com>
2016-11-29 22:01:28 -0500

    Initial Commit

---
+++ MaJiangCalc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MaJiangCalc
+//
+//  Created by Meng Huang on 11/29/16.
+//  Copyright © 2016 Meng Huang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
